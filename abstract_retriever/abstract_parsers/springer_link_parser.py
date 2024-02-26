d6d713b0f6caf434f395162dae29ee639cd5049b
melonkernel <toffe@bskog.com>
2024-02-26 13:39:43 +0200

    fix bug in springer link

--- abstract_retriever/abstract_parsers/springer_link_parser.py
+++ abstract_retriever/abstract_parsers/springer_link_parser.py
@@ -1,5 +1,5 @@
 from abstract_retriever.abstract_parsers.abstract_parser import AbstractParser
 
 class SpringerLinkParser(AbstractParser):
-    URL_PREFIX = "https://link.springer.com/article/10.1007/s40948-023-00721-1"
+    URL_PREFIX = "https://link.springer.com/article/"
     ABSTRACT_SELECTOR = "#Abs1-content p"
