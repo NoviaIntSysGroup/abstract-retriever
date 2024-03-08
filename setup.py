97d1d63ebbfa1fdbfecee95b8e2ce75102d1a493
melonkernel <toffe@bskog.com>
2024-03-08 09:13:01 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'beautifulsoup4==4.10.0'
-        'requests==2.26.0'
-        'pytest'
-        'python-dotenv'
-        'pyppeteer'
-        'pyppeteer_stealth'
+        'beautifulsoup4==4.10.0',
+        'requests==2.26.0',
+        'pytest',
+        'python-dotenv',
+        'pyppeteer',
+        'pyppeteer_stealth',
         'pytest-asyncio'
     ],
 )
